0f70d97b74b0d32722cc0ee23f27f54975dad750
Beckwith <12001003566749@FEDIDCARD.GOV>
2018-12-12 09:38:04 -0700

    Added preprocessor print methods for error checking.

--- z-SickDecoder/lms511/aaMain.cpp
+++ z-SickDecoder/lms511/aaMain.cpp
@@ -5,6 +5,14 @@
 #include "PointCloudBuilder.h"
 #include "MergeTime.h"
 
+#define CATCH 1
+#define PROGRESS 1
+
+#if PROGRESS
+	int countLidarLines(std::string fineName);
+#endif
+
+
 using namespace std;
 
 int main()
@@ -17,10 +25,26 @@ int main()
 
 	gps.setOffsetDist(1);
 
+	#if PROGRESS
+	int currentScan=0;
+	int currentStep=0;
+	int totalScans=countLidarLines("zrawLidar.txt");
+	#endif
+
+
 	while(getline(lidarFile,line))
 	{
 		try
 		{
+		#if PROGRESS
+		currentStep ++;
+		if(currentScan>currentStep)
+		{
+			std::cout<<"\t\t\tProgress: "<<currentScan<<" of "<<totalScans<<endl;
+			currentStep ++;
+		}
+		#endif
+
 		MergeTime transmitionTime;
 		transmitionTime.setStamp(line);
 
@@ -34,15 +58,37 @@ int main()
 		builder.addPoints(lidar.getDistValues(),lidar.getStartAngle(), lidar.getAngularStep());
 		builder.rotateRow(location.getHeading());
 		builder.placeRow(location.getNorthing(), location.getEasting());
+
 		}
 		catch(const char * e)
 		{
-
+			#if CATCH
+				cout <<e<<endl<<endl;
+			#endif
 		}
-		catch(...){}
+		catch(...)
+		{
+			#if CATCH
+				cout <<"Unexpected Error."<<endl<<endl;
+			#endif
+		}
 	}
 	
 	builder.writeFile("Test.pcd");
 
 	return 0;
 }
+
+
+#if PROGRESS
+int countLidarLines(std::string fileName)
+{
+	int count =0;
+	std::ifstream lidar;
+	std::string line;
+	for(lidar.open(fileName); getline(lidar,line); count++){}
+	lidar.close();
+	return count;
+}
+
+#endif
